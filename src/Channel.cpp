47f32048056de8dd8c18f7c07a31fd7e2567f4d4
uzxiu <uzxiu@student.kit.edu>
2019-12-05 22:14:13 +0100

    update channel

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -4,4 +4,72 @@
 
 #include "Channel.h"
 
-nio::Channel::Channel(EventLoop* loop,int fd){}
+nio::Channel::Channel(EventLoop* loop,int fd):
+                            loop_(loop),
+                            fd_(0),
+                            events_(0),
+                            revents_(0),
+                            lastEvents_(0){
+
+
+}
+
+void nio::Channel::setFd(int fd) { fd_=fd; }
+
+int nio::Channel::getFd() { return fd_; }
+
+void nio::Channel::setEvents(__uint32_t event) {
+    events_=event;
+}
+void nio::Channel::setRevents(__uint32_t event) {
+    revents_=event;
+}
+
+void nio::Channel::handelEvents() {
+    events_=0;
+    if((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)){
+        return;
+    }
+
+    if (revents_ & EPOLLERR) {
+        return;
+    }
+
+    if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) {
+        handleRead();
+    }
+    if (revents_ & EPOLLOUT) {
+        handleWrite();
+    }
+    handleConn();
+}
+
+void nio::Channel::handleRead() {
+    if(readHandler_)
+        readHandler_();
+}
+
+void nio::Channel::handleWrite() {
+    if(writeHandler_)
+        writeHandler_();
+}
+
+void nio::Channel::handleConn() {
+    if(connHandler_)
+        connHandler_();
+}
+
+void nio::Channel::setReadHandler(CallBack &&readHandler) {
+    readHandler_ = readHandler;
+}
+
+void nio::Channel::setWriteHandler(CallBack &&writeHandler) {
+    writeHandler_=writeHandler;
+}
+
+void nio::Channel::setConnHandler(CallBack &&connHandler) {
+    connHandler_=connHandler;
+}
+
+
+nio::Channel::~Channel() = default;
--- src/Channel.h
+++ src/Channel.h
@@ -6,7 +6,7 @@
 #define SFS_SERVER_CHANNEL_H
 
 #include <sys/epoll.h>
-
+#include <functional>
 
 namespace nio {
 
@@ -15,13 +15,40 @@ class EventLoop;
 class Channel {
 
 public:
+    typedef std::function<void()> CallBack;
+
     Channel(EventLoop *loop, int fd);
+
+    int getFd();
+    void setFd(int fd);
+
+    void setEvents(__uint32_t event);
+    void setRevents(__uint32_t event);
+    void handelEvents();
+
+    void setReadHandler(CallBack &&readHandler);
+    void setWriteHandler(CallBack &&writeHandler);
+    void setConnHandler(CallBack &&connHandler);
+
+    void handleRead();
+    void handleWrite();
+    void handleConn();
 
     ~Channel();
 
 
 private:
 
+    EventLoop *loop_;
+    int fd_;
+
+    __uint32_t events_;
+    __uint32_t revents_;
+    __uint32_t lastEvents_;
+
+    CallBack readHandler_;
+    CallBack writeHandler_;
+    CallBack connHandler_;
 };
 
 
